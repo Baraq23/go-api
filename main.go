dc5fb7f112858637f41a9b34d72c3dba56bc87fb
barraotieno <barrackotieno160@gmail.com>
2025-04-26 22:44:45 +0300

    feat: add get event by id functionality, refactor code: reorganize files to respective modules.

--- main.go
+++ main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"goapi/db"
-	"goapi/models"
-	"net/http"
+	"goapi/routes"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,38 +11,7 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 
-	server.GET("/events", getEvents)
-	server.POST("/events", createEvent)
+	routes.RegisteredRoutes(server)
 
 	server.Run(":8080")
-}
-
-func getEvents(context *gin.Context) {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events. Try agin later."})
-		return
-	}
-	context.JSON(http.StatusOK, events)
-}
-
-
-func createEvent(context *gin.Context) {
-	var event models.Event
-	err := context.ShouldBindJSON(&event)
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
-		return
-	}
-
-	event.ID = 1
-	event.UserID = 1
-
-	err = event.Save()
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not savethe event. Try agin later."})
-		return
-	}
-
-	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
