9efc9f9a8cf61e5b2452ee05d7a26bd5b15cb02a
barraotieno <barrackotieno160@gmail.com>
2025-04-29 11:05:21 +0300

    Feat: add authentication middleware that is called in the protected route.

--- controllers/eventControllers.go
+++ controllers/eventControllers.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"goapi/models"
-	"goapi/utils"
 	"net/http"
 	"strconv"
 
@@ -38,30 +37,14 @@ func GetEvent(context *gin.Context) {
 
 func CreateEvent(context *gin.Context) {
 
-	token := context.Request.Header.Get("Authorization")
-
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not Authorized."})
-		return
-	}
-
-	userId, err := utils.VerifyToken(token)
-
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "could not validate token."})
-		return
-	}
-
-
-
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
 		return
 	}
 
-	
+	userId := context.GetInt64("userId")
 	event.UserID = userId
 
 	err = event.Save()
--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"goapi/controllers"
+	"goapi/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,7 +10,7 @@ import (
 func RegisteredRoutes(server *gin.Engine) {
 	server.GET("/events", controllers.GetEvents)
 	server.GET("/events/:id", controllers.GetEvent)
-	server.POST("/events", controllers.CreateEvent)
+	server.POST("/events", middlewares.Authenticate, controllers.CreateEvent)
 	server.PUT("/events/:id", controllers.UpdateEvent) //PUT is used to Update
 	server.DELETE("/events/:id", controllers.DeleteEvent)
 	server.POST("/signup", controllers.Signup)
